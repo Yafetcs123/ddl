94aae8fc048d1fb3692409ed6ab53fcace2f0496
Yafetcs123 <yafetperfecto@gmail.com>
2025-06-08 00:31:01 +0700

    first commit

--- anime/urls.py
+++ anime/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Ubah 'home/' menjadi '' untuk halaman utama
     path('list', views.list, name='list'),  # Ubah 'home/' menjadi '' untuk halaman utama
-    path('anime/<int:anime_id>/', views.detail, name='anime_detail'),
+    path('anime/<int:anime_id>/', views.detail, name='detail'),
     path('anime/<int:anime_id>/watch/<int:episode_num>/', views.watch, name='watch_episode'),
 ]
